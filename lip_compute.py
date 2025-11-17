5748b8ce7a974476ef6f580a9e1da50032711772
xuebashuoge <sugarhe58@gmail.com>
2025-11-17 17:19:16 +1100

    1117 revise Lipschitz compute

--- lip_compute.py
+++ lip_compute.py
@@ -41,6 +41,7 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+
     # set device and seed
     device = set_device()
     set_seed(args.seed, device)
@@ -77,8 +78,8 @@ if __name__ == '__main__':
     # compute Lipschitz constant
     lip_constant = compute_lipschitz_constant_new(args, loader=lip_test_loader, mc_samples=args.mc_samples, pmin=args.pmin, clamping=args.clamping, chunk_size=args.chunk_size, init_net=net0, device=device)
 
-    print(f'Computed Lipschitz constant: {lip_constant}')
-    
+    print(f'Computed Lipschitz constant, {args.name_data}, {args.model}-{args.layers}, {args.init_prior}-prior_{args.channel_type.lower()}, {channel_specs}_chan-layer{args.l_0}, {'bounded' if args.clamping else 'unbounded'}-loss: {lip_constant}')
+
     # save Lipschitz constant
     with open(f'results/lip_constant_{args.lip_name}_{args.name_data}_{args.model}-{args.layers}_{args.init_prior}-prior_{args.channel_type.lower()}_{channel_specs}_chan-layer{args.l_0}_{'bounded' if args.clamping else 'unbounded'}-loss.json', 'w') as f:
         json.dump({'lip_constant': lip_constant}, f)
