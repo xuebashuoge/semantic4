6ecab5c15fd21c25dba841b1f64fc44daf7f988b
xuebashuoge <1172508020@qq.com>
2025-11-19 13:18:48 +0800

    1119 4090

--- lip_compute_4090.sh
+++ lip_compute_4090.sh
@@ -2,18 +2,18 @@
 
 # Script to compute Lipschitz constant for a given model and dataset
 
-python lip_compute.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist
+python lip_compute.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --gpu 1
 
-python lip_compute.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist
+python lip_compute.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --gpu 1
 
-python lip_compute.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist
+python lip_compute.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --gpu 1
 
-python lip_compute.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist
+python lip_compute.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --gpu 1
 
-python lip_compute.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist
+python lip_compute.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --gpu 1
 
-python lip_compute.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist
+python lip_compute.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --gpu 1
 
-python lip_compute.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist
+python lip_compute.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --gpu 1
 
-python lip_compute.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist
+python lip_compute.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --gpu 1
