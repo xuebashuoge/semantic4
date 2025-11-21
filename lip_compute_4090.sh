52026a45040596a85d9bc36ad44cc4eb66d616ac
xuebashuoge <1172508020@qq.com>
2025-11-21 12:41:44 +0800

    1119 4090, Lipschitz rayleigh results

--- lip_compute_4090.sh
+++ lip_compute_4090.sh
@@ -18,34 +18,50 @@
 
 # python lip_compute_4090.py --name_data mnist --model cnn --layers 4 --init_prior random --l_0 2 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 150
 
-# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 9 --init_prior learnt --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 9 --init_prior learnt --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 9 --init_prior random --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 9 --init_prior random --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type bec --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type bec --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 9 --init_prior learnt --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 9 --init_prior learnt --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 9 --init_prior random --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
 
-python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+python lip_compute_4090.py --name_data cifar10 --model cnn --layers 9 --init_prior random --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+
+# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+
+# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior learnt --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+
+# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+
+# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 13 --init_prior random --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+
+# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+
+# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior learnt --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+
+# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type rayleigh --clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
+
+# python lip_compute_4090.py --name_data cifar10 --model cnn --layers 15 --init_prior random --l_0 4 --channel_type rayleigh --no-clamping --lip_name test-set_mc-dist --lip_bs 250 --chunk_size 250 --gpu 2 --mc_samples 200
